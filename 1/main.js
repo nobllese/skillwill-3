f8df03fb2bd386c4f854b939a9ea3cea17986866
Tamar Jafaridze <116025220+nobllese@users.noreply.github.com>
2023-07-07 16:49:01 +0400

    Update main.js

--- 1/main.js
+++ 1/main.js
@@ -1,22 +1,21 @@
 const numbers = (...args) => {
     const calculateSum = (num1, num2) => {
-        return num1 + num2;
+      return num1 + num2;
     };
-
+    
     const calculateMultiplication = (...args) => {
-        let total = 1;
-        for (const num of args) {
-            total *= num;
-        }
-        return total;
+      let total = 1;
+      for (let i = 2; i < args.length; i++) { 
+        total *= args[i];
+      }
+      return total;
     };
-
-
+    
     const first = args[0];
     const second = args[1];
-
-    return[calculateSum(first, second), calculateMultiplication(...args)];
-}
-
-console.log(numbers(7, 11, 13, 17, 19, 23));
-
+    
+    return [calculateSum(first, second), calculateMultiplication(...args)];
+  };
+  
+  console.log(numbers(1, 2, 3, 4, 5, 6));
+  
